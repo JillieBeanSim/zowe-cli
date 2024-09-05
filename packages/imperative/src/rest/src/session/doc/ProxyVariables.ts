fa94ad6938c53a54ccfbf3cca31b13c55cba9956
Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
2024-09-05 16:17:39 -0400

    save state
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>

--- packages/imperative/src/rest/src/session/doc/ProxyVariables.ts
+++ packages/imperative/src/rest/src/session/doc/ProxyVariables.ts
@@ -13,13 +13,23 @@ export interface ProxyVariables {
     /**
      * HTTP_PROXY/http_proxy value
      */
-    http_proxy: string,
+    http_proxy?: string,
     /**
      * HTTPS_PROXY/https_proxy value
      */
-    https_proxy: string,
+    https_proxy?: string,
     /**
      * string[] of NO_PROXY/no_proxy values
      */
-    no_proxy: string[]
+    no_proxy?: string[],
+    /**
+     * The value to send as the Proxy-Authorization
+     * header for every network request
+     */
+    proxy_authorization?: string,
+    /**
+     * boolean value for whether the proxy server certificate
+     * should be verified against the list of supplied CAs
+     */
+    proxy_strict_ssl?: boolean
 }
